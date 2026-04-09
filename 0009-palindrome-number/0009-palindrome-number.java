103ef2e8aee20db2c7c57602ee794f3ba845ac4d
SATHISH IT <sathishkpr2006@gmail.com>
2026-04-09 17:54:03 +0530

    Time: 5 ms (79.44%), Space: 44.2 MB (99.99%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+        return false;
+        int rev=0;
+        int originalnumber=x;
+        while(x!=0){
+            int rem=x%10;
+            rev=rev*10+rem;
+            x/=10;
+        }
+        return originalnumber==rev;
+    }
+}
