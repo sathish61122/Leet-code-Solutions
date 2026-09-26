eba8c77307d108965da660b6ea5930e490f981c0
SATHISH IT <sathishkpr2006@gmail.com>
2026-09-26 07:45:34 +0530

    Refactor median finding logic for sorted arrays

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,21 +3,16 @@ class Solution {
           if (nums1.length > nums2.length) {
             return findMedianSortedArrays(nums2, nums1);
         }
-
         int m = nums1.length;
         int n = nums2.length;
         int low = 0, high = m;
-
         while (low <= high) {
             int partitionX = (low + high) / 2;
             int partitionY = (m + n + 1) / 2 - partitionX;
-
             int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
             int minRightX = (partitionX == m) ? Integer.MAX_VALUE : nums1[partitionX];
-
             int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
             int minRightY = (partitionY == n) ? Integer.MAX_VALUE : nums2[partitionY];
-
             if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
                 // Correct partition
                 if ((m + n) % 2 == 0) {
